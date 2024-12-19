97fe273b06f7bb09e33b158a701a35fa7f7cafc8
PaolaVotano <paolavotano56@gmail.com>
2024-12-19 20:56:58 +0100

    struttura modificata
    card - img - promo carousel - footer

--- project work/js/home.js
+++ project work/js/home.js
@@ -23,32 +23,3 @@ const progressCircle = document.querySelector(".autoplay-progress svg");
       }
     });
     
-
-    const carouselContainer = document.querySelector('.carousel-container');
-const carouselItems = document.querySelectorAll('.carousel-item');
-const prevBtn = document.querySelector('.carousel-btn.prev');
-const nextBtn = document.querySelector('.carousel-btn.next');
-
-let currentIndex = 0;
-
-const updateCarousel = () => {
-    const scrollPosition = carouselItems[currentIndex].offsetLeft - carouselContainer.offsetWidth / 2 + carouselItems[currentIndex].offsetWidth / 2;
-
-    carouselContainer.scrollTo({
-        left: scrollPosition
-    });
-};
-
-// btn precedente
-prevBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex === 0) ? carouselItems.length - 1 : currentIndex - 1;
-    updateCarousel();
-});
-
-// btn successivo
-nextBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex === carouselItems.length - 1) ? 0 : currentIndex + 1;
-    updateCarousel();
-});
-
-updateCarousel();
