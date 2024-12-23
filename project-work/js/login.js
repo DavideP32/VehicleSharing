fb6225ee9b3f25c0a145f3e1de7370e8b82adafd
Davide <183734626+DavideP32@users.noreply.github.com>
2024-12-24 00:22:08 +0100

    login.js

---
+++ project-work/js/login.js
@@ -0,0 +1,42 @@
+let form = document.querySelector("form");
+
+form.addEventListener("submit", e =>{
+    e.preventDefault();
+
+    const email = form.email.value.trim();
+    const password = form.password.value.trim();
+
+    const utente = {
+        email: email,
+        password: password
+    };
+
+
+    fetch("http://localhost:8080/api/utente/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(utente),
+        credentials: 'include'
+
+    })
+    .then(response =>{
+        console.log('Raw response:', response);
+        if (!response.ok) {
+            console.error('Errore durante il login:', response.status, response.statusText);
+            throw new Error("Login fallito");
+        }
+        return response.json()
+    })
+    .then(utenteData => {  
+        console.log('Login riuscito:', utenteData);
+        localStorage.setItem('utente', JSON.stringify(utenteData));
+        // window.location.href = '/impostazioni.html'; 
+        window.location.replace('http://127.0.0.1:5500/project-work/index.html');
+    })
+    .catch(error => {
+        console.error('Errore durante il login:', error);
+        alert('Login fallito. Controlla le credenziali.');
+    });
+})
